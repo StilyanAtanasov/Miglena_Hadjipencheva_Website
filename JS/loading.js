0c7ae75bc566e45ef2d6b165a284e35a804d6934
Stilyan Atanasov <stilyan2008@gmail.com>
2025-07-09 16:31:50 +0300

    Removed old design files
    - Preserved some design temporaly in Home/Index

--- JS/loading.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  hideLoadingScreen();
-});
-
-function hideLoadingScreen() {
-  var loadingScreen = document.getElementById("loading-screen");
-  loadingScreen.style.display = "none";
-}
