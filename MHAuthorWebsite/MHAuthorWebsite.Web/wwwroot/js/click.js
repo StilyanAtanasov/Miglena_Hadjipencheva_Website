b895eaaa4bc8ab2b78852a34b1ba1640a4b37d6d
Stilyan Atanasov <stilyan2008@gmail.com>
2025-07-15 11:28:47 +0300

    Make entire product card clickable in liked products page
    Also: add hover animation and improve the style of product card

--- MHAuthorWebsite/MHAuthorWebsite.Web/wwwroot/js/click.js
+++ MHAuthorWebsite/MHAuthorWebsite.Web/wwwroot/js/click.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll(".product").forEach(function (div) {
+  document.querySelectorAll(".clickable").forEach(function (div) {
     div.addEventListener("click", function (e) {
-      // Prevent navigation if a button, link, or form element was clicked
       if (e.target.closest("a, button, form")) return;
       window.location = div.getAttribute("data-details-url");
     });
