95f6dd868358cfeef8c5a38cf0066993a4ba9ca3
Stilyan Atanasov <stilyan2008@gmail.com>
2025-07-16 11:42:27 +0300

    Add initial state of cart page.

---
+++ MHAuthorWebsite/MHAuthorWebsite.Web.ViewModels/Cart/CartItemViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace MHAuthorWebsite.Web.ViewModels.Cart;
+
+public class CartItemViewModel
+{
+    public Guid ItemId { get; set; }
+
+    public Guid ProductId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Category { get; set; } = null!;
+
+    public decimal UnitPrice { get; set; }
+
+    public int Quantity { get; set; }
+
+    public bool IsDiscontinued { get; set; }
+
+    public bool IsAvailable { get; set; }
+
+    public decimal LineTotal => UnitPrice * Quantity;
+}
---
+++ MHAuthorWebsite/MHAuthorWebsite.Web.ViewModels/Cart/CartViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace MHAuthorWebsite.Web.ViewModels.Cart;
+
+public class CartViewModel
+{
+    public List<CartItemViewModel> Items { get; set; } = new();
+    public decimal Total => Items.Where(i => i is { IsAvailable: true, IsDiscontinued: false }).Sum(i => i.LineTotal);
+}
