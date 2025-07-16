c09884be66fb436f2923f4f29badeb5b319df113
Stilyan Atanasov <stilyan2008@gmail.com>
2025-07-16 16:14:45 +0300

    Make the whole cartItem clickable in Cart page

--- MHAuthorWebsite/MHAuthorWebsite.Web/wwwroot/js/click.js
+++ MHAuthorWebsite/MHAuthorWebsite.Web/wwwroot/js/click.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.querySelectorAll(".clickable").forEach(function (div) {
     div.addEventListener("click", function (e) {
-      if (e.target.closest("a, button, form")) return;
+      if (e.target.closest("a, button, input")) return;
       window.location = div.getAttribute("data-details-url");
     });
   });
