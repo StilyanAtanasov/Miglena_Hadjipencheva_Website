7934a392363c1425005b52df9a7a1c0584408477
Stilyan Atanasov <stilyan2008@gmail.com>
2025-07-20 15:11:50 +0300

    Improve code quality around sort button

--- MHAuthorWebsite/MHAuthorWebsite.GCommon/ApplicationRules.cs
+++ MHAuthorWebsite/MHAuthorWebsite.GCommon/ApplicationRules.cs
@@ -6,4 +6,19 @@ public static class ApplicationRules
     {
         public const byte PageSize = 10;
     }
+    public static class SortTypes
+    {
+        public const string Recommended = "recommended";
+        public const string PriceDesc = "price_desc";
+        public const string PriceAsc = "price_asc";
+        public const string Likes = "likes";
+
+        public static readonly Dictionary<string, string> DisplayNames = new()
+        {
+            [Recommended] = "Предложени",
+            [PriceDesc] = "Цена: низходящо",
+            [PriceAsc] = "Цена: възходящо",
+            [Likes] = "Харесвания"
+        };
+    }
 }
--- MHAuthorWebsite/MHAuthorWebsite.Web/Utils/SortValueMapper.cs
+++ MHAuthorWebsite/MHAuthorWebsite.Web/Utils/SortValueMapper.cs
@@ -1,5 +1,6 @@
 ﻿using MHAuthorWebsite.Data.Models;
 using System.Linq.Expressions;
+using static MHAuthorWebsite.GCommon.ApplicationRules.SortTypes;
 
 namespace MHAuthorWebsite.Web.Utils;
 
@@ -8,9 +9,9 @@ public static class SortValueMapper
     public static readonly Dictionary<string, (bool descending, Expression<Func<Product, object>>? expression)> SortMap =
         new()
         {
-            ["recommended"] = (true, null),
-            ["price_desc"] = (true, p => p.Price),
-            ["price_asc"] = (false, p => p.Price),
-            ["likes"] = (true, p => p.Likes.Count)
+            [Recommended] = (true, null),
+            [PriceDesc] = (true, p => p.Price),
+            [PriceAsc] = (false, p => p.Price),
+            [Likes] = (true, p => p.Likes.Count)
         };
 }
