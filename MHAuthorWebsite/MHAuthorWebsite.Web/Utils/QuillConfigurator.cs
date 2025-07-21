932de26022d01bc645a05198368ca729edca8e75
Stilyan Atanasov <stilyan2008@gmail.com>
2025-07-21 12:43:32 +0300

    FIx Quill's visualisation being broken due to HtmlSanitizer removing important attributes

---
+++ MHAuthorWebsite/MHAuthorWebsite.Web/Utils/QuillConfigurator.cs
@@ -0,0 +1,19 @@
+﻿using Ganss.Xss;
+
+namespace MHAuthorWebsite.Web.Utils;
+
+public static class QuillConfigurator
+{
+    public static HtmlSanitizer GetQuillHtmlSanitizer()
+    {
+        return new HtmlSanitizer
+        {
+            AllowedTags =
+            {
+                "p", "br", "strong", "em", "ul", "ol", "li", "a", "img",
+                "h1", "h2", "h3", "h4", "h5", "h6"
+            },
+            AllowDataAttributes = true,
+        };
+    }
+}
