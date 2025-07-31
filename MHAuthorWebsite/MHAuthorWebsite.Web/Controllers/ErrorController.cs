3f2850db9193b36cc979871f00d6bcff06a0b3a0
Stilyan Atanasov <stilyan2008@gmail.com>
2025-07-31 11:25:48 +0300

    Add error page for StatusCode 400 - BadRequest

--- MHAuthorWebsite/MHAuthorWebsite.Web/Controllers/ErrorController.cs
+++ MHAuthorWebsite/MHAuthorWebsite.Web/Controllers/ErrorController.cs
@@ -13,6 +13,7 @@ public class ErrorController : BaseController
     public IActionResult Error(int? statusCode) =>
         statusCode switch
         {
+            400 => View("400"),
             401 => View("401"),
             403 => View("403"),
             404 => View("404"),
