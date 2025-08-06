7ff38c03e191e9a54b7ac5d3957f87f7dfcaaae8
Stilyan Atanasov <stilyan2008@gmail.com>
2025-08-06 15:10:20 +0300

    Add styling to Profile page

---
+++ MHAuthorWebsite/MHAuthorWebsite.Web/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
@@ -0,0 +1,16 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+#nullable disable
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MHAuthorWebsite.Web.Areas.Identity.Pages.Account.Manage
+{
+    public class EmailModel : PageModel
+    {
+        public IActionResult OnGet() => RedirectToAction("Index", "Home");
+
+        public IActionResult OnPost() => RedirectToAction("Index", "Home");
+    }
+}
