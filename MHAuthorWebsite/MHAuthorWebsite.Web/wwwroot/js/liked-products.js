9aef897b26ad5ab8f7a1cf0248649e1394788055
Stilyan Atanasov <stilyan2008@gmail.com>
2025-08-10 13:52:32 +0300

    Fix no message is shown when the last product is removed from the liked products list

--- MHAuthorWebsite/MHAuthorWebsite.Web/wwwroot/js/liked-products.js
+++ MHAuthorWebsite/MHAuthorWebsite.Web/wwwroot/js/liked-products.js
@@ -2,8 +2,13 @@
 
 import { pushNotification } from "./notification.js";
 
+let count;
+
 document.addEventListener(`DOMContentLoaded`, function () {
-  document.querySelectorAll(`[data-role="remove-item"]`).forEach(b =>
+  const likedProducts = document.querySelectorAll(`[data-role="remove-item"]`);
+  count = likedProducts.length;
+
+  likedProducts.forEach(b =>
     b.addEventListener(`click`, async function () {
       const itemId = b.dataset.itemId;
 
@@ -18,6 +23,8 @@ document.addEventListener(`DOMContentLoaded`, function () {
         b.closest(`.product-card`).remove();
         const line = document.getElementById(`line-${itemId}`);
         if (line != null) line.remove();
+
+        if (--count === 0) document.getElementById(`zero-liked-message`).classList.remove(`hidden`);
 
         pushNotification("Продуктът е премахнат успешно!", "success");
       } else pushNotification("Грешка при премахването на продукта!", "error");
