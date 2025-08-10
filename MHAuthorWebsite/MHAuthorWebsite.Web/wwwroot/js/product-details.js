2947d12d5d166a700d94663190a1446555f737b4
Stilyan Atanasov <stilyan2008@gmail.com>
2025-08-10 13:00:54 +0300

    Implement AJAX for like in product details as well

--- MHAuthorWebsite/MHAuthorWebsite.Web/wwwroot/js/product-details.js
+++ MHAuthorWebsite/MHAuthorWebsite.Web/wwwroot/js/product-details.js
@@ -1,7 +1,27 @@
 import { initQuill } from "./editor.js";
+import { pushNotification } from "./notification.js";
 
 document.addEventListener(`DOMContentLoaded`, async function () {
   await initQuill(false, false);
+
+  document.querySelectorAll(`[data-role="remove-item"]`).forEach(b =>
+    b.addEventListener(`click`, async function () {
+      const itemId = b.dataset.itemId;
+
+      const response = await fetch(`/Product/ToggleLike/${itemId}`, {
+        method: "POST",
+        headers: {
+          RequestVerificationToken: document.querySelector('input[name="__RequestVerificationToken"]').value,
+        },
+      });
+
+      if (response.ok) {
+        const isAdded = b.classList.toggle(`liked`);
+
+        pushNotification(isAdded ? `Продуктът е харесан успешно!` : `Продуктът е премахнат от харесани!`, `success`);
+      } else pushNotification(`Възникна неочаквана грешка!`, `error`);
+    })
+  );
 });
 
 // --- Slider ---
