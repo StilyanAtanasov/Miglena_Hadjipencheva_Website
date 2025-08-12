5f8898355addbb3b754ed55d2b54aca0301100d5
Stilyan Atanasov <stilyan2008@gmail.com>
2025-08-12 11:36:54 +0300

    Change app's default name in authenticator app.
    Also:
    - Fix Logout button hover styles when in profile page

--- MHAuthorWebsite/MHAuthorWebsite.Web/Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
+++ MHAuthorWebsite/MHAuthorWebsite.Web/Areas/Identity/Pages/Account/Manage/EnableAuthenticator.cshtml.cs
@@ -183,7 +183,7 @@ namespace MHAuthorWebsite.Web.Areas.Identity.Pages.Account.Manage
             return string.Format(
                 CultureInfo.InvariantCulture,
                 AuthenticatorUriFormat,
-                _urlEncoder.Encode("Microsoft.AspNetCore.Identity.UI"),
+                _urlEncoder.Encode("Миглена Хаджипенчева"),
                 _urlEncoder.Encode(email),
                 unformattedKey);
         }
