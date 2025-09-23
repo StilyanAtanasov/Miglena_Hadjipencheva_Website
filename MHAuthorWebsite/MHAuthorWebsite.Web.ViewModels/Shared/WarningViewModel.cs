1e41283d6074c82dfba21b5ed33c883777f36ddd
Stilyan Atanasov <stilyan2008@gmail.com>
2025-09-23 16:59:22 +0300

    Finish styling order details page

---
+++ MHAuthorWebsite/MHAuthorWebsite.Web.ViewModels/Shared/WarningViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MHAuthorWebsite.Web.ViewModels.Shared;
+
+public class WarningViewModel
+{
+    public string Title { get; set; } = "Внимание: ";
+
+    public string Message { get; set; } = null!;
+}
